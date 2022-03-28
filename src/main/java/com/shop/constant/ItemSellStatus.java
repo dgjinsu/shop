925b61b7ff5ba024f3f6ba97c85b0d8859dc8954
dgjinsu <rlawlstn3206@naver.com>
2022-03-28 11:15:34 +0900

    쇼핑몰 프로젝트

---
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -0,0 +1,9 @@
+package com.shop.constant;
+
+/**
+ * 현재 판매 중인지 품절 상태인지 나타냄
+ */
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,41 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString
+@Entity
+@Table(name = "item") //엔티티와 매핑할 테이블을 지정
+public class Item {
+
+    @Id
+    @Column(name="item_id") //필드와 컬럼 매핑
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;                        //상품코드
+
+    @Column(nullable = false, length = 50)  //null 값 불허, 길이 50제한
+    private String itemNm;                  //상품명
+
+    @Column(name = "price", nullable = false)
+    private int price;                      //가격
+
+    @Column(nullable = false)
+    private int stockNumber;                //재고수량
+
+    @Lob //BLOB,CLOB 타입 매핑(문자형 대용량 파일을 저장하는데 사용)
+    @Column(nullable = false)
+    private String itemDetail;              //상품상세설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;  //상품 판매 상태
+
+    private LocalDateTime regTime;          //등록 시간
+
+    private LocalDateTime updateTime;       //수정 시간
+}

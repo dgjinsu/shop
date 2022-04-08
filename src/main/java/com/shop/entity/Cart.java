67c282f26b95e1ed53eb2a2076327b7206671b86
dgjinsu <rlawlstn3206@naver.com>
2022-04-08 23:08:00 +0900

    쇼핑몰 프로젝트

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -17,6 +17,7 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    // forein key FK
     @OneToOne
     @JoinColumn(name = "member_id")
     private Member member;

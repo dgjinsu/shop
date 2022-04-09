361c8d96bd1c2b42022994347306f3735338a025
dgjinsu <rlawlstn3206@naver.com>
2022-04-09 23:34:43 +0900

    쇼핑몰 프로젝트

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -23,7 +25,11 @@ public class Order {
     private LocalDateTime orderDate;    //주문일
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
+    private OrderStatus orderStatus;    //주문상태
+
+    @OneToMany(mappedBy = "order")      // 연관 관계의 주인은 OrderItem 이다.
+    private List<OrderItem> orderItems = new ArrayList<>();
+
 
     private LocalDateTime regTime;          //등록 시간
 

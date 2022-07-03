2b543bd65fb326e3ebe6c66a78b8e2e2023ee54b
dgjinsu <rlawlstn3206@naver.com>
2022-07-03 14:18:24 +0900

    쇼핑몰 프로젝트

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -56,6 +58,19 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/admin/item/{itemId}")
+    public String itemDT1(@PathVariable("itemId") Long itemId, Model model){
+        try{
+            ItemFormDto itemFormDto = itemService.getItemDt1(itemId);
+            model.addAttribute("itemFormDto", itemFormDto);
+        } catch(EntityNotFoundException e){
+            model.addAttribute("errorMessage", "존재하지 않는 상품 입니다");
+            model.addAttribute("itemFormDto", new ItemFormDto());
+            return "item/itemForm";
+        }
+        return "item/itemForm";
     }
 
 
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -11,7 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Log
 @Service
@@ -20,32 +24,52 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
     private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
 
     @Autowired
-    public ItemService(ItemRepository itemRepository, ItemImgService itemImgService, ItemImgRepository itemImgRepository) {
+    public ItemService(ItemRepository itemRepository, ItemImgService itemImgService, ItemImgRepository itemImgRepository, ItemImgRepository itemImgRepository1) {
         this.itemRepository = itemRepository;
         this.itemImgService = itemImgService;
+        this.itemImgRepository = itemImgRepository1;
     }
 
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
 
         //상품 등록
-        Item item = itemFormDto.createItem();
+        Item item = itemFormDto.createItem(); //상품 등록 폼으로부터 받은 데이터를 이용하여 item 객체 생성
         itemRepository.save(item);
         //이미지 등록
         for(int i=0;i<itemImgFileList.size();i++){
             ItemImg itemImg = new ItemImg();
             itemImg.setItem(item);
 
-            if(i == 0)
+            if(i == 0) //첫번째 이미지를 대표 이미지로
                 itemImg.setRepImgYn("Y");
-            else
+            else //나머지는 대표 이미지 x
                 itemImg.setRepImgYn("N");
 
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+        return item.getId();
+    }
+    //itemid : 30
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDt1(Long itemId){
 
+        Item item = itemRepository.findById(itemId) //itemRepository 에서 조회
+                .orElseThrow(EntityNotFoundException::new);
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemOrderByIdAsc(item);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+        //itemImgList -> itemImgDtoList
+        for(ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
         }
 
-        return item.getId();
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
     }
 }

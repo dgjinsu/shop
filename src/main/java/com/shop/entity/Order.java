be601578caf2c6cabc06bfa54083174a916f0d02
dgjinsu <rlawlstn3206@naver.com>
2022-07-14 23:43:13 +0900

    쇼핑몰 프로젝트

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -3,6 +3,7 @@ package com.shop.entity;
 import com.shop.constant.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -33,6 +34,27 @@ public class Order extends BaseEntity{
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+        Order order = new Order();
+        order.setMember(member);
+        for(OrderItem orderItem : orderItemList) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
 
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice(); //하나의 아이템 주문 엔티티의 총 가격을 전체 주문 엔티티 전체가격에 더함
+        }
+        return totalPrice;
+    }
 }
